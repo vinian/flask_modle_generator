d55fa7cb829fe026ca84c10d3f76545ba52efb7f
weinianhua <weinianhua@baidu.com>
2016-11-08 22:45:29 +0800

    parse xml

--- fm_generaot.py
+++ fm_generaot.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # Author: Nianhua,Wei(willian.wnh@gmail.com)
 
+'''
+read database schema from xml file, and dump it to sqlqlchemy models.py
+'''
+
 import xml.etree.ElementTree as ET
 import sys
 import os
@@ -18,7 +22,21 @@ if not os.path.isfile(xml_file):
 
 tree = ET.parse(xml_file)
 
-print tree
+for elem in tree.iter(tag='table'):
+    for sub_elem in elem.iter(tag='row'):
+        print sub_elem.items()
+        for item in ['datatype', 'default']:
+            for child in sub_elem.iter(tag='{0}'.format(item)):
+                print child.tag, child.text
+
+
+
+
+
+
+
+
+# TODO: parse tree data, convert it to python code
 
 
 
